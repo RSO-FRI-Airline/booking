47e76d6743b2c76773bc28b292012cace07e5338
Jaka Kordez <jaka.kordez@gmail.com>
2019-01-13 20:45:43 +0100

    Testing mailing

--- api/src/main/java/si/fri/rso/BookingApplication.java
+++ api/src/main/java/si/fri/rso/BookingApplication.java
@@ -1,6 +1,8 @@
 package si.fri.rso;
 
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -8,5 +10,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("v1")
+@RegisterService
 public class BookingApplication extends Application {
 }
